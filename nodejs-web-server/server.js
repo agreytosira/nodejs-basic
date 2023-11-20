0b182720cbf4eeab38922b6b4e51ee5a56fc83ba
Agrey Tosira <agreytosira@gmail.com>
2023-11-20 20:10:08 +0700

    update response status

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -2,17 +2,19 @@ const http = require('http')
 
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html')
-  response.statusCode = 200
   const { url, method } = request
 
   if (url === '/') {
     if (method === 'GET') {
+      response.statusCode = 200
       response.end('Ini adalah homepage')
     } else {
+      response.statusCode = 400
       response.end(`Halaman tidak dapat diakses dengan ${method} request`)
     }
   } else if (url === '/about') {
     if (method === 'GET') {
+      response.statusCode = 200
       response.end('Halo! Ini adalah halaman about')
     } else if (method === 'POST') {
       let body = []
@@ -24,12 +26,15 @@ const requestListener = (request, response) => {
       request.on('end', () => {
         body = Buffer.concat(body).toString()
         const { name } = JSON.parse(body)
+        response.statusCode = 200
         response.end(`Halo, ${name}! Ini adalah halaman about`)
       })
     } else {
+      response.statusCode = 400
       response.end(`Halaman tidak dapat diakses dengan ${method} request`)
     }
   } else {
+    response.statusCode = 404
     response.end(`Halaman tidak ditemukan!`)
   }
 }

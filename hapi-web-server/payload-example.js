5cbd8f76d74a33540afbe71052feed33fb99320e
Agrey Tosira <agreytosira@gmail.com>
2023-11-24 07:09:17 +0700

    add hapijs handler h example

---
+++ hapi-web-server/payload-example.js
@@ -0,0 +1,12 @@
+// Detailed notation
+const handler = (request, h) => {
+    const response = h.response('success');
+    response.type('text/plain');
+    response.header('X-Custom', 'some-value');
+    return response;
+};
+
+// Chained notation
+const handler = (request, h) => {
+    return h.response('success').type('text/plain').header('X-Custom', 'some-value');
+};
